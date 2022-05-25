9b88f97a9f4b011805202afffb29eebe19c81a07
hoangtoeic <mr.hoangenglish@gmail.com>
2022-05-25 21:45:37 +0700

    CreateEnvironmentPyAndConvertFilePy

--- app.py
+++ app.py
@@ -1,13 +1,24 @@
 
    
 from flask import Flask, redirect, url_for, render_template, request
-
+import pandas as pd
 app = Flask(__name__)
 
+def recommend(code):
+    print('call recommend')
+    dataset=pd.read_csv("product_cnpm.csv", engine='python', error_bad_lines=False)
+    dataset.head(130)
+    print('dataset', dataset)
+    return code
 
-@app.route("/")
+@app.route("/evaluate", methods=['POST'])
 def hello_world():
-    return "hello world"
+    data = request.json
+    code = data['name']
+    result = {
+        "name": "ok"
+    }
+    return recommend(result)
 
 if __name__ == "__main__":
-    app.run(debug=True)   
+    app.run(port=8080,debug= True)         
