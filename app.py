f949217c3a2dd1fcc75b4b903f01f242bfb3b5a4
hoangtoeic <mr.hoangenglish@gmail.com>
2022-05-28 14:16:01 +0700

    fix recommend system output

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 
    
 from flask import Flask, redirect, url_for, render_template, request
+import time
+
 # import pandas as pd
 # from sklearn.feature_extraction.text import TfidfVectorizer
 # from sklearn.metrics.pairwise import cosine_similarity
@@ -103,7 +105,7 @@ async def recommendRoot(code):
     import pandas as pd
 
     # %%
-    dataset=pd.read_csv("product_cnpm.csv", engine='python', error_bad_lines=False)
+    dataset= pd.read_csv("product_cnpm.csv", engine='python', error_bad_lines=False)
 
     # %%
     dataset.head(130)
@@ -118,21 +120,21 @@ async def recommendRoot(code):
     # Only stars and score are in float rest are objects
 
     # %%
-    def important_features(dataset):
-        data=dataset.copy()
+    async def important_features(dataset):
+        data= dataset.copy()
         for i in range(0, dataset.shape[0]):
             data["imp"]=data["name"]+' '+data["description"]
         return data
 
 
     # %%
-    data=important_features(dataset)
+    data=await important_features(dataset)
 
     # %%
     #data.head(20)
 
     # %%
-    data["ids"]=[i for i in range(0,data.shape[0])]
+    data["ids"]= [i for i in range(0,data.shape[0])]
 
     # %%
     #data["ids"].head(10000)
@@ -146,8 +148,8 @@ async def recommendRoot(code):
     vec=TfidfVectorizer()
 
     # %%
-    vecs=vec.fit_transform(data["imp"].apply(lambda x: np.str_(x)))
-
+    vecs= vec.fit_transform(data["imp"].apply(lambda x: np.str_(x)))
+    # print("vecs", vecs)
     # %%
     #vecs.shape
 
@@ -158,7 +160,8 @@ async def recommendRoot(code):
     from sklearn.metrics.pairwise import cosine_similarity
 
     # %%
-    sim=cosine_similarity(vecs) 
+    sim= cosine_similarity(vecs) 
+    # print("sim", sim)
 
     # %%
     #sim.shape##Columns represent ids and not rows
@@ -222,7 +225,7 @@ async def recommendRoot(code):
 
 
     # %%
-    def recommend(name):
+    async def recommend(name):
         # product_id=data[data.name==name]["ids"].values[0]
         # print("product_id", product_id)
 
@@ -236,23 +239,24 @@ async def recommendRoot(code):
             mostSimilar = similarName
             product_id = data[data.name == eachName]["ids"].values[0]
 
-    #    print('ids', product_id)
+        print('product_id', product_id)
 
-        scores=list(enumerate(sim[product_id]))
+        scores= list(enumerate(sim[product_id]))
     #    print("scores", scores)
-        sorted_scores=sorted(scores,key=lambda x:x[1],reverse=True)
+        sorted_scores= sorted(scores,key=lambda x:x[1],reverse=True)
     # print("sorted_scores", sorted_scores)
         sorted_scores=sorted_scores[1:]
         # categoryproducts = 
-        
+        # print("sorted_scores", sorted_scores)
         # categoryproducts=[data[products[0]==data["ids"]]["categories"].values[0] for products in sorted_scores]
         # categoryproduct = [data[tempt[0]==data["categories"][product_id]]["title"].values[0] for tempt in categoryproducts]
         sorted_Score_By_Categories = []
         for products in sorted_scores:
             catogoryEachProduct = data[products[0]==data["ids"]]["category_id"].values[0]   
-        if catogoryEachProduct == data["category_id"][product_id]:
-            sorted_Score_By_Categories.append (products)
+            if catogoryEachProduct == data["category_id"][product_id]:
+                sorted_Score_By_Categories.append (products)
         sorted_Title=[data[products[0]==data["ids"]]["name"].values[0] for products in sorted_Score_By_Categories]      
+        # print("sorted_Title", sorted_Title)
         return sorted_Title
 
     # %%
@@ -267,14 +271,17 @@ async def recommendRoot(code):
         return first_ten
 
     # %%
-    lst=recommend("Laptop APPLE MacBook Pro 2020 MYD82SA/A ( 13.3"" Apple M1/8GB/256GB SSD/macOS/1.4kg)")
-    #print ("lst", lst)
-    m=recommend_ten(lst)
+    lst=await recommend("Laptop APPLE MacBook Pro 2020 MYD82SA/A ( 13.3"" Apple M1/8GB/256GB SSD/macOS/1.4kg)")
+    print ("lst", lst)
+    m= recommend_ten(lst)
+    # time.sleep(10)
 
     # %%
     print(m)
-
-    return m
+    result = {
+        "name": str(m)
+    }
+    return result
 
 
 @app.route("/evaluate", methods=['POST'])
@@ -282,9 +289,9 @@ async def hello_world():
     data = request.json
     code = data['name']
     ok = await recommendRoot(code)
-    result = {
-        "name": ok
-    }
+    # result = {
+    #     "name": ok
+    # }
     return ok
 
 if __name__ == "__main__":
