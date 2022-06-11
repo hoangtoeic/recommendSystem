dad7ee346af13edeacf85c0fa8d0f9d2d7d41f84
Nguyen Hoang <hoangthanh.nguyen@team.enouvo.com>
2022-06-11 15:52:52 +0700

    update algorithm with exceptProductId

--- app.py
+++ app.py
@@ -7,103 +7,9 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 
-# import pandas as pd
-# from sklearn.feature_extraction.text import TfidfVectorizer
-# from sklearn.metrics.pairwise import cosine_similarity
-# from difflib import SequenceMatcher
-
-
-# import numpy as np
 app = Flask(__name__)
 
-
-
-# def similar(a, b):
-#     return SequenceMatcher(None, a, b).ratio()
-
-# def important_features(dataset):
-#     data=dataset.copy()
-#     for i in range(0, dataset.shape[0]):
-#         data["imp"]=data["name"]+' '+data["description"]
-#     return data
-
-
-# def recommend_ten(product_list):
-#     first_ten=[]
-#     count=0
-#     for product in product_list:
-#         if count > 9:
-#             break
-#         count+=1
-#         first_ten.append(product)
-#     return first_ten
-
-
-# def recommend(name,data):
-#     # product_id=data[data.name==name]["ids"].values[0]
-#     # print("product_id", product_id)
-
-    
-#     mostSimilar = 0
-#     product_id = 1
-
-#     for eachName in data.name:
-#       similarName = similar(eachName, name)
-#       if similarName > mostSimilar:
-#         mostSimilar = similarName
-#         product_id = data[data.name == eachName]["ids"].values[0]
-
-# #    print('ids', product_id)
-
-#     scores=list(enumerate(sim[product_id]))
-# #    print("scores", scores)
-#     sorted_scores=sorted(scores,key=lambda x:x[1],reverse=True)
-#    # print("sorted_scores", sorted_scores)
-#     sorted_scores=sorted_scores[1:]
-#     # categoryproducts = 
-    
-#     # categoryproducts=[data[products[0]==data["ids"]]["categories"].values[0] for products in sorted_scores]
-#     # categoryproduct = [data[tempt[0]==data["categories"][product_id]]["title"].values[0] for tempt in categoryproducts]
-#     sorted_Score_By_Categories = []
-#     for products in sorted_scores:
-#       catogoryEachProduct = data[products[0]==data["ids"]]["category_id"].values[0]   
-#       if catogoryEachProduct == data["category_id"][product_id]:
-#         sorted_Score_By_Categories.append (products)
-#     sorted_Title=[data[products[0]==data["ids"]]["name"].values[0] for products in sorted_Score_By_Categories]      
-#     return sorted_Title
-
-
-# def recommendRoot(code):
-#     print('call recommend')
-#     dataset=pd.read_csv("product_cnpm.csv", engine='python', error_bad_lines=False)
-#     dataset.head(130)
-#     data=important_features(dataset)
-#     data["ids"]=[i for i in range(0,data.shape[0])]
-#     vec=TfidfVectorizer()
-#     vecs=vec.fit_transform(data["imp"].apply(lambda x: np.str_(x)))
-#     sim=cosine_similarity(vecs) 
-#     scores=list(enumerate(sim[4]))
-#     sorted_scores=sorted(scores,key=lambda x:x[1],reverse=True)
-#     sorted_scores=sorted_scores[1:]
-#     name = 'Chuột gaming CorSAIR Ironclaw'
-#     mostSimilar = 0
-#     mostSimilarIds = 1
-#     for eachName in data.name:
-#         similarName = similar(eachName, name)
-#         if similarName > mostSimilar:
-#             mostSimilar = similarName
-#             mostSimilarIds = data[data.name == eachName]["ids"]
-    
-#     lst=recommend("Laptop APPLE MacBook Pro 2020 MYD82SA/A ( 13.3"" Apple M1/8GB/256GB SSD/macOS/1.4kg)",data)
-#     #print ("lst", lst)
-#     m=recommend_ten(lst)
-
-#     # %%
-#     print(m)
-#     # print('dataset', dataset)
-#     return code
-
-async def recommendRoot(id):
+async def recommendRoot(id, exceptProductID):
     # %%
 ##Import Libraries
     # %%
@@ -111,15 +17,6 @@ async def recommendRoot(id):
 
     # %%
     dataset.head(130)
-
-    # %%
-    #dataset.shape
-
-    # %%
-    #dataset.info()
-
-    # %% [markdown]
-    # Only stars and score are in float rest are objects
 
     # %%
     async def important_features(dataset):
@@ -139,69 +36,18 @@ async def recommendRoot(id):
     data["ids"]= [i for i in range(0,data.shape[0])]
 
     # %%
-    #data["ids"].head(10000)
-
-    # %%
-    ##Importing vectoriser
-
+    
     # %%
     vec=TfidfVectorizer()
 
     # %%
     vecs= vec.fit_transform(data["imp"].apply(lambda x: np.str_(x)))
-    # print("vecs", vecs)
-    # %%
-    #vecs.shape
-
-    # %%
-    #print (vecs)
-
-    # %%
-
-    # %%
+   
     sim= cosine_similarity(vecs) 
-    # print("sim", sim)
-
-    # %%
-    #sim.shape##Columns represent ids and not rows
-
-    # %%
-    #print (sim)
-
-    # %%
-    # product_id=data[data.title=="Energizer 2 in 1 Light"]["ids"].values[0]
-    # print (product_id)
-
-    # %%
+    
     scores=list(enumerate(sim[4]))
     sorted_scores=sorted(scores,key=lambda x:x[1],reverse=True)
     sorted_scores=sorted_scores[1:]
-
-    #print (sorted_scores)
-
-    # %%
-    # print(data["categories"][5])
-    # print ((data[products[0]==data["ids"]]["categories"].values[0])==data["categories"][product_id])
-    # print (data["categories"][product_id])
-
-    # %%
-    # for products in sorted_scores:
-    #     {
-    #         print (products[0])
-    #         }
-
-
-    # %%
-    # data[data.Name=="The Matrix"].ids.values[0]
-    # product_id=data[data.name=="Laptop APPLE MacBook Air 2020 MGN63SA/A ( 13.3"" Apple M1/8GB/256GB SSD/macOS/1.3kg"]
-    # print("product_id", product_id)
-
-    # df = pd.DataFrame(data)
-    # pd.options.display.max_colwidth = 100
-    # print(df['name'])
-
-    # Laptop APPLE MacBook Pro 2020 MYD82SA/A ( 13.3" Apple M1/8GB/256GB SSD/macOS/1.4kg)
-    # Laptop APPLE MacBook Pro 2020 MYD82SA/A ( 13.3"" Apple M1/8GB/256GB SSD/macOS/1.4kg)",3200000.0,NULL,9440
 
     # %%
     def similar(a, b):
@@ -217,43 +63,20 @@ async def recommendRoot(id):
             mostSimilar = similarName
             mostSimilarIds = data[data.name == eachName]["ids"]
             
-    #print('ok',mostSimilar)
-    #print('ids', mostSimilarIds)
-
-
+  
     # %%
     async def recommend(product_id):
-        # product_id=data[data.name==name]["ids"].values[0]
-        # print("product_id", product_id)
-
-        
-        # mostSimilar = 0
-        # product_id = 2
-
-        # for eachName in data.name:
-        #     similarName = similar(eachName, name)
-        # if similarName > mostSimilar:
-        #     mostSimilar = similarName
-        #     product_id = data[data.name == eachName]["ids"].values[0]
-
-        # print('product_id', product_id)
-
+       
         scores= list(enumerate(sim[product_id]))
-    #    print("scores", scores)
         sorted_scores= sorted(scores,key=lambda x:x[1],reverse=True)
-    # print("sorted_scores", sorted_scores)
+   
         sorted_scores=sorted_scores[1:]
-        # categoryproducts = 
-        # print("sorted_scores", sorted_scores)
-        # categoryproducts=[data[products[0]==data["ids"]]["categories"].values[0] for products in sorted_scores]
-        # categoryproduct = [data[tempt[0]==data["categories"][product_id]]["title"].values[0] for tempt in categoryproducts]
         sorted_Score_By_Categories = []
         for products in sorted_scores:
             catogoryEachProduct = data[products[0]==data["ids"]]["category_id"].values[0]   
             if catogoryEachProduct == data["category_id"][product_id]:
                 sorted_Score_By_Categories.append (products)
         sorted_Title=[data[products[0]==data["ids"]]["ids"].values[0] for products in sorted_Score_By_Categories]      
-        # print("sorted_Title", sorted_Title)
         print("sorted_Score_By_Categories", sorted_Score_By_Categories)
 
         return sorted_Title
@@ -266,14 +89,16 @@ async def recommendRoot(id):
             if count > 9:
                 break
             count+=1
-            first_ten.append(product)
+            if product in exceptProductID:
+                continue
+            else:
+                first_ten.append(product)
         return first_ten
 
     # %%
     lst=await recommend(id)
     print ("lst", lst)
     m= recommend_ten(lst)
-    # time.sleep(10)
 
     # %%
     print(m)
@@ -286,11 +111,10 @@ async def recommendRoot(id):
 @app.route("/recommend", methods=['POST'])
 async def hello_world():
     data = request.json
-    code = data['id']
-    ok = await recommendRoot(code)
-    # result = {
-    #     "name": ok
-    # }
+    productId = data['id']
+    exceptProductID = data['exceptProductID']
+    print ("exceptProductID", exceptProductID)
+    ok = await recommendRoot(productId, exceptProductID)
     return ok
 
 if __name__ == "__main__":
